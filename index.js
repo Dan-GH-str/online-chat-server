6f83555d843d41730155542d2c34dbd2dd266b70
Dan-GH-str <danzhuff80603@mail.ru>
2023-12-28 18:56:57 +0300

    Added Headers

--- index.js
+++ index.js
@@ -57,5 +57,11 @@ io.on('connection', (socket) => {
 
 app.use(cors({ origin: "*" }))
 app.use(router)
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
 
 server.listen(PORT, () => console.log(`Server has been started on PORT ${PORT}`))

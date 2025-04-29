408907869c4cdb93087bc167140bcb1c03ea6767
Dan-GH-str <danzhuff80603@mail.ru>
2025-04-29 11:08:48 +0300

    Cookies options corrected

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,7 +50,7 @@ const login = async (req, res) => {
         const cookieOptions = {
             httpOnly: true, // Предотвращает доступ JavaScript
             // secure: process.env.NODE_ENV === 'production', // Только для HTTPS в production
-            sameSite: 'Strict', //  Защита от CSRF (лучше всего 'Strict', но необходимы доп настройки???)
+            sameSite: 'Lax', //  Защита от CSRF (лучше всего 'Strict', но необходимы доп настройки???)
             maxAge: process.env.JWT_EXPIRATION * 1000, // Срок действия cookie (в миллисекундах)
             path: '/' // Куда можно отправлять этот cookie
         }
